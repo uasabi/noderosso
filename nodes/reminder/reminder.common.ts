b725b66436e9df89705b41c6f20b04ef0770a427
Abdelrahman Abdelrahman <git@abogic.al>
2021-03-28 02:05:29 +0200

    Add reminder node

---
+++ nodes/reminder/reminder.common.ts
@@ -0,0 +1,21 @@
+import * as z from 'zod'
+
+const Schema = {
+  tick: z.object({
+    _msgid: z.string(),
+    topic: z.literal('TICK.V1'),
+  })
+}
+
+export const actions = Schema.tick
+export type Actions = z.infer<typeof actions>
+export const isAction = actions.check.bind(actions)
+export function upgradeAction(action: any, log: (message: string) => void): z.infer<typeof actions> {
+  return action
+}
+
+export const events = z.void();
+export type Events = ReturnType<typeof Event[keyof typeof Event]>
+export const isEvent = events.check.bind(events)
+
+export const Event = {}
