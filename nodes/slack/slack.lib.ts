f1f32b08713d6b0db81360e77095a68e4a7386bd
chewcw <chwai87@gmail.com>
2021-07-03 00:08:45 +0000

    Fixed XPath issue in Slack node
    XPath selector with channel name was not reliable, now the Slack node
    expects a channel's url link instead of channel name.

--- nodes/slack/slack.lib.ts
+++ nodes/slack/slack.lib.ts
@@ -13,57 +13,69 @@ export function Setup(setupArg: SetupArg) {
   return async (action: Actions, send: (event: Events) => void, done: () => void) => {
     switch (action.topic) {
       case 'SEND.V1': {
-        const { message, channel, workspace } = action.payload
+        const { message, channelLink } = action.payload
         const { node, accountName, accountPassword } = setupArg
         let browser
 
         try {
-          const url = `https://${workspace}.slack.com`
           browser = await puppeteer.connect({ browserWSEndpoint: 'ws://localhost:3000' })
           const page = await browser.newPage()
 
-          await page.goto(url)
-          const title = await page.$eval('title', (el) => el.textContent)
-          if (title?.includes(`There's been a glitch`)) {
-            throw `Workspace ${workspace} not found.`
-          }
+          await page.goto(channelLink)
+          node.log(`Navigating to ${channelLink}`)
 
           await page.focus('#email')
           await page.keyboard.type(accountName)
+          node.log('Typing email')
 
           await page.focus('#password')
           await page.keyboard.type(accountPassword)
+          node.log('Typing password')
 
           await Promise.all([page.click('#signin_btn'), page.waitForNavigation()])
+          node.log(`Signing in`)
 
           await page.waitForTimeout(1000)
 
-          // select specific channel
-          const selector = `//span[@data-qa='channel_sidebar_name_${channel}']/parent::div/parent::div`
-          await page.waitForXPath(selector)
-          const [targetElement] = await page.$x(selector)
-          if (!targetElement) {
-            throw `Specified channel's name ${channel} dom element not found.`
+          const title = await page.$eval('title', (el: any) => el.textContent)
+          if (title?.includes(`There's been a glitch`)) {
+            throw `Invalid channel ${channelLink}`
           }
-          await targetElement.click()
+
+          // get archive id
+          if (!channelLink.includes('/archives')) {
+            throw 'Channel link should include /archives'
+          }
+          const archiveId = channelLink.substring(channelLink.search('/archives') + 10)
+          node.log(`The channel's archive id is ${archiveId}`)
+
+          // click open with browser link
+          const browserSelector = `a[href='/messages/${archiveId}']`
+          node.log(`Looking for \`${browserSelector}\``)
+          await page.waitForSelector(browserSelector)
+          await page.click(browserSelector)
+          node.log(`Navigated to ${channelLink}`)
 
           // write and send messsage
           await page.waitForSelector('.ql-editor')
+          node.log('Focus on message editor')
           await page.focus('.ql-editor')
+          node.log('Typing message')
           await page.keyboard.type(message)
+          node.log('Click send message button')
           await page.click('button[data-qa="texty_send_button"]')
 
           browser.disconnect()
 
-          node.log(`Processed slack message to ${channel}`)
+          node.log(`Processed slack message to ${channelLink}`)
           node.status({
             fill: 'green',
             shape: 'dot',
-            text: `Last processed ${workspace}: ${action.payload.channel} ${time()}`,
+            text: `Last processed send message to ${channelLink} ${time()}`,
           })
         } catch (e) {
           browser?.disconnect()
-          const message = [`Error writing message to channel ${channel}`, JSON.stringify(e)].join('\n')
+          const message = [`Error writing message to channelLink ${channelLink}`, e].join('\n')
 
           send(Event.failed({ message }))
 
